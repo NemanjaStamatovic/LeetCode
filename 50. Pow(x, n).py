e0cb0f5af55c9e0014d23ce0d2908fb87f90cb68
NemanjaStamatovic <75085688+NemanjaStamatovic@users.noreply.github.com>
2021-01-25 22:13:37 +0100

    Add files via upload

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -1,18 +1,31 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        res = 1
+        if n == 0:
+            return 1
         
-        if n > 0:
-            sign = 1
-        else:
-            sign = -1
-            n = -n
+        if n < 0:
+            return self.myPow(1/x, -n)
         
-        for i in range(n):
-            res *= x
+        half = self.myPow(x, n//2)
+        
+        if n % 2 == 1:
+            return half * half * x
+        
+        return half * half
+        
+#         res = 1
+        
+#         if n > 0:
+#             sign = 1
+#         else:
+#             sign = -1
+#             n = -n
+        
+#         for i in range(n):
+#             res *= x
             
-        if sign == -1:
-            res = 1/res
+#         if sign == -1:
+#             res = 1/res
             
-        return res    
+#         return res    
