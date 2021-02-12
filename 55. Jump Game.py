27089cff074859b43a9094904faefb91fb01f631
NemanjaStamatovic <75085688+NemanjaStamatovic@users.noreply.github.com>
2021-02-12 18:18:32 +0100

    Add files via upload

---
+++ 55. Jump Game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        maximum = 0
+        
+        for i in range(len(nums)):
+            
+            if i > maximum:
+                return False
+            
+            maximum = max(maximum, i + nums[i])
+            
+        return True    
